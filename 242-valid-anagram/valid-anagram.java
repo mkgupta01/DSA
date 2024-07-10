b5f0e1031a45afd94ea04f25744e1b81af3133ff
Mayank Kumar Gupta <66107976+mkgupta01@users.noreply.github.com>
2024-07-10 11:57:34 +0530

    Time: 25 ms (7.62%) | Memory: 44.6 MB (20.61%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -2,12 +2,18 @@ import java.util.Arrays;
 
 class Solution {
     public boolean isAnagram(String s, String t) {
-        char[] sChars = s.toCharArray();
-        char[] tChars = t.toCharArray();
+        if(s.length() != t.length()){
+            return false;
+        }
+        HashMap<Character, Integer> sMap = new HashMap<>();
+        HashMap<Character, Integer> tMap = new HashMap<>();
+
+        for(int i=0;i<s.length();i++){
+            sMap.put(s.charAt(i), sMap.getOrDefault(s.charAt(i),0)+1);
+            tMap.put(t.charAt(i), tMap.getOrDefault(t.charAt(i),0)+1);
+        }
+
+        return sMap.equals(tMap);
         
-        Arrays.sort(sChars);
-        Arrays.sort(tChars);
-        
-        return Arrays.equals(sChars, tChars);
     }
 }
