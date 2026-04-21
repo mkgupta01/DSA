910f71b169c26902e011fbe706d4fbeab12eb88a
Mayank Kumar Gupta <66107976+mkgupta01@users.noreply.github.com>
2026-04-21 22:11:18 +0530

    Time: 1 ms (86.38%) | Memory: 44.2 MB (88.34%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,23 +1,26 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> ans = new ArrayList<>();
-        helper("", n, 0, 0, ans);
+        helper(new StringBuilder(), n, 0, 0, ans);
         return ans;
     }
 
-    static void helper(String p, int k, int cC, int oC, List<String> ans){
-        //cC = closing count depict number of closing brackets
-        //oC = opening count depicts numbe rof opening brackets        
-        if(p.length() == k*2){
-            ans.add(p);
+    static void helper(StringBuilder p, int k, int cC, int oC, List<String> ans){
+        if(p.length() == k * 2){
+            ans.add(p.toString());
             return;
         }
 
         if(oC < k){
-            helper(p+"(", k ,cC, oC+1, ans);
+            p.append('(');
+            helper(p, k, cC, oC + 1, ans);
+            p.deleteCharAt(p.length() - 1);
         }
+
         if(cC < oC){
-            helper(p+")", k, cC+1,oC, ans);
+            p.append(')');
+            helper(p, k, cC + 1, oC, ans);
+            p.deleteCharAt(p.length() - 1);
         }
     }
 }
